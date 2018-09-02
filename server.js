64d28a13e332ff9056873f4e8914cf2ae2e62ae2
Al Sade <alsade15@gmail.com>
2018-09-02 11:57:19 +0300

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.set('view engine', 'hbs');
+// app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to home page',
+        currentYear: new Date().getFullYear()
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}...`);
+});
